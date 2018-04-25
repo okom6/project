f23e06c0720b48c4fcdfb6aacbc44f967ee6f15c
Michał Górnik <36916061+okom6@users.noreply.github.com>
2018-04-25 19:28:13 +0200

    Update zadanie3.py

--- Zajecia 8/zadanie3.py
+++ Zajecia 8/zadanie3.py
@@ -5,11 +5,11 @@ import subprocess
 import os
 
 dirStr='''K1
-    K2
-    K3
-        K4
+	K2
+	K3
+		K4
 K5
-    K6'''
+	K6'''
 
 komenda="mkdir"
 s1=""
@@ -17,20 +17,22 @@ s2=""
 s3=""
 l=0
 
-for(i=0; i<dirStr.size(); ++i):
-    if(dirStr[i]!="/t"):
-        if(l==0)
-            s1=dirStr[i]
-            komenda=komenda+" "+s1
-        elif(l==1)
-            s2=dirStr[i]
-            komenda=komenda+" "+s1+"/"+s2
-        else:
-            s1=dirStr[i]
-            komenda=komenda+" "+s1+"/"+s2+"/"+s3
-        l=0
-    else:
-        ++l
-        
+for i in dirStr :
+	if(i!="\t"):
+		if(i!="\n"):
+			if(i!="K"):
+				print(l)
+				if(l==0):
+					s1=i
+					komenda=komenda+" K"+s1
+				elif(l==1):
+					s2=i
+					komenda=komenda+" K"+s1+"/K"+s2
+				else:
+					s3=i
+					komenda=komenda+" K"+s1+"/K"+s2+"/K"+s3
+				l=0
+	else:
+		l=l+1
 
 subprocess.check_output([komenda], shell=True)
